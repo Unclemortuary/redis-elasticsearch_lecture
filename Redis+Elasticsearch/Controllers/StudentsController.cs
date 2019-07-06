a96bfb38e4b3c09d7603f8a5f771cc3363c5f43d
Artem Gusev <Artem_Gusev@epam.com>
2019-07-06 15:19:06 +0300

    memory cahce examples

---
+++ Redis+Elasticsearch/Controllers/StudentsController.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Distributed;
+using Microsoft.Extensions.Caching.Memory;
+using Redis_Elasticsearch.Models;
+using StackExchange.Redis;
+
+namespace Redis_Elasticsearch.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentsController : ControllerBase
+    {
+        private readonly IMemoryCache memoryCache;
+        private readonly IDistributedCache distributedCache;
+
+        private static readonly ConnectionMultiplexer multiplexer = ConnectionMultiplexer.Connect("localhost");
+
+        public StudentsController(IMemoryCache memoryCache, IDistributedCache distributedCache)
+        {
+            this.memoryCache = memoryCache;
+            this.distributedCache = distributedCache;
+        }
+
+        // GET api/values
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new[] { "Students" };
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public ActionResult<Student> Get(int id)
+        {
+            return memoryCache.Get<Student>(id);
+        }
+
+        // POST api/values
+        [HttpPost]
+        public int Post([FromBody] Student student)
+        {
+            var random = new Random();
+            int newId = random.Next();
+            //compex calculations
+            //...
+            //...
+            //...
+            if (!memoryCache.TryGetValue(newId, out Student cachedStudent))
+            {
+                memoryCache.Set(newId, new Student(newId, student.Name, student.Surname));
+            }
+            return newId;
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+            
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ Redis+Elasticsearch/Models/Student.cs
@@ -0,0 +1,16 @@
+﻿namespace Redis_Elasticsearch.Models
+{
+    public class Student
+    {
+        public int Id { get; }
+        public string Name { get; }
+        public string Surname { get; }
+
+        public Student(int id, string name, string surname)
+        {
+            Id = id;
+            Name = name;
+            Surname = surname;
+        }
+    }
+}
