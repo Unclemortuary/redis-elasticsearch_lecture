afcebcbdcdc7576ebed246c48462c18b2f8c4dc8
Artem Gusev <Artem_Gusev@epam.com>
2019-07-06 17:13:56 +0300

    singleton via lazy

---
+++ Redis+Elasticsearch/Helpers/RedisConnectionHelper.cs
@@ -0,0 +1,13 @@
+﻿using StackExchange.Redis;
+using System;
+
+namespace Redis_Elasticsearch.Helpers
+{
+    public static class RedisConnectionHelper
+    {
+        private static Lazy<ConnectionMultiplexer> lazyConnection = new Lazy<ConnectionMultiplexer>(
+            () => ConnectionMultiplexer.Connect("localhost"));
+
+        public static ConnectionMultiplexer Connection => lazyConnection.Value;
+    }
+}
