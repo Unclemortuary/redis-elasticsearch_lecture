c9084d956280e063bd424d4f085be61b72038569
Artem Gusev <Artem_Gusev@epam.com>
2019-07-10 20:39:56 +0300

    cleanup

--- Redis+Elasticsearch/Startup.cs
+++ Redis+Elasticsearch/Startup.cs
@@ -26,7 +26,6 @@ namespace Redis_Elasticsearch
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
-            //services.AddMemoryCache();
             services.AddDistributedRedisCache(options =>
             {
                 options.Configuration = "localhost";
